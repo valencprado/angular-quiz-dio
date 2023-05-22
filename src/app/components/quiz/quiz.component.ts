1123b3257c375990d817793649bfda6fee818a17
Valen <valenpschool@gmail.com>
2023-05-22 16:43:09 -0300

    feat: lógica do componente

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import quiz_questions from '../../../assets/data/quiz_questions.json'
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
   styleUrls: ['./quiz.component.css']
 })
-export class QuizComponent {
+export class QuizComponent implements OnInit {
+  title: string = '';
+  questions: any = [];
+  questionSelected: any = '';
+  answers: string[] = [];
+  answerSelected:string = '';
+  questionIndex:number = 0;
+  questionMaxIndex: number = 0;
+  finished: boolean = false;
+  
+  ngOnInit(): void {
+    if(quiz_questions) {
+      this.finished = false;
+      this.title = quiz_questions.title;
+      this.questions = quiz_questions.questions;
+      this.questionSelected = this.questions[this.questionIndex];
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.questions.length;
+    }
+  }
+
+  playerChoice(value: string) {
+    this.answers.push(value)
+  }
+ 
+  async nextStep() {
+    this.questionIndex++;
+    if(this.questionMaxIndex > this.questionIndex) {
+      this.questionSelected = this.questions[this.questionIndex]
+    } else {
+      this.finished = true;
+    }
+  }
 
 }
